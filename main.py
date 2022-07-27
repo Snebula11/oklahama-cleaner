f91f3e6613df2ca59b87f716611b9ec58e347fba
Snebula11 <benjamin@swedberg.us>
2022-07-27 16:06:20 -0500

    trying to perfect ca output

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import pandas as pd
-
 import ballotpedia as bp
 import ctcl
 
@@ -10,15 +8,20 @@ if __name__ == '__main__':
         state = input('Type the postal abbr. of the state you want: ').upper()
 
         if state == 'CA':
-            url = 'https://raw.githubusercontent.com/Snebula11/oklahama-cleaner/main/all_california_candidates.csv'
-            data = bp.pd.read_csv(url)
-            df = bp.pd.DataFrame(data)
-            bp.convert_ballotpedia(df).to_csv('out_bp.csv', index=False)
+            # getting ballotpedia data
+            bp_url = 'https://raw.githubusercontent.com/Snebula11/oklahama-cleaner/main/data' \
+                     '/all_california_candidates.csv '
+            bp_data = bp.pd.read_csv(bp_url)
+            bp_df = bp.pd.DataFrame(bp_data)
+            # creating filepath
+            output_filepath = 'data/' + state.lower() + '_output.csv'
+            # output california data
+            bp.convert_ballotpedia(bp_df).to_csv(output_filepath, index=False)
             break
         elif state == 'OK':
             data = ctcl.pd.read_csv('/Users/benswedberg/Desktop/oklahama-cleaner/OK_candidates.csv')
             df = ctcl.pd.DataFrame(data)
-            ctcl.candidates(df).to_csv('out_ctcl.csv', index=False)
+            ctcl.candidates(df).to_csv('data/out_ctcl.csv', index=False)
             break
         else:
             print("\nSorry, we couldn't find that state. Please try again using a two-letter postal code.\n")
