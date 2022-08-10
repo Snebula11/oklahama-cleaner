276a36d8f2d885de60724966dc66b96cadacc53e
Snebula11 <benjamin@swedberg.us>
2022-08-10 14:09:16 -0500

    fixed accidentally using incorrect past converted_bp, updated data

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ import urllib.error
 
 if __name__ == '__main__':
     available_states = ['AZ', 'CA', 'GA', 'MA', 'IL', 'MD', 'MS', 'NY', 'OK']
+    converted_bp = converted_ctcl = converted_propublica = converted_openstates = None
     go = False
 
     # input loop
     # for state in util.states.keys():
-    while True:
+    for state in util.states.keys():
 
-        state = input('Type the postal abbr. of the state you want: ').upper()
+        # state = input('Type the postal abbr. of the state you want: ').upper()
 
         # OUTPUT FILEPATH
         output_filepath = 'data/' + state.upper() + '/' + state.lower() + '_output.csv'
@@ -59,20 +60,19 @@ if __name__ == '__main__':
 
         # OUTPUT MERGED DATASET
         # check if we have all 3
-        try:
+        if converted_bp is not None:
+            print(f'unifying 4 datasets for {state}')
             unified = unify.unify_bp_ctcl(converted_bp,
                                           converted_ctcl,
                                           third_df=converted_openstates,
                                           fourth_df=converted_propublica)
+            unified.to_csv(output_filepath, index=False, encoding='utf-8')
         # if we do not
-        except NameError:
-            print(f'unifying datasets for {state}')
-            unified = unify.unify_bp_ctcl(converted_ctcl, converted_openstates, third_df=converted_propublica)
+        elif converted_bp is None:
+            print(f'unifying 3 datasets for {state}')
+            unified = unify.unify_bp_ctcl(converted_ctcl,
+                                          converted_openstates,
+                                          third_df=converted_propublica)
             unified.to_csv(output_filepath, index=False, encoding='utf-8')
-            # pass
-            # break
-        # if we do have all 3
-        else:
-            print(f'unifying datasets for {state}')
-            unified.to_csv(output_filepath, index=False, encoding='utf-8')
-            # break
+
+        converted_bp = converted_ctcl = converted_propublica = converted_openstates = None
